6fa26641ee791eba31f11baf01c7fa1a7af96a7a
Ivan Szebenszki <szib77@gmail.com>
2019-03-03 17:17:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  # console.log(board.inspect)
   board.map! { |tile| " #{tile} " }
     .map!.with_index { |tile, idx| idx % 3 === 1 ? "|#{tile}|" : tile }
-    console.log('hey')
-  console.log(board)
+  puts "#{board[0]}#{board[1]}#{board[2]}"
+  puts "-" * 11
+  puts "#{board[0]}#{board[1]}#{board[2]}"
+  puts "#{board[0]}#{board[1]}#{board[2]}"
 end
