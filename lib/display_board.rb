a21dc8bb9ddccc0eb4cc1bebc880afefc0781a4f
Ivan Szebenszki <szib77@gmail.com>
2019-03-03 17:17:33 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,5 +6,6 @@ def display_board(board)
   puts "#{board[0]}#{board[1]}#{board[2]}"
   puts "-" * 11
   puts "#{board[0]}#{board[1]}#{board[2]}"
+  puts "-" * 11
   puts "#{board[0]}#{board[1]}#{board[2]}"
 end
