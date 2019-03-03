aaf5ab98f1fb52b3c4dce45f605f5b21040ca35f
Ivan Szebenszki <szib77@gmail.com>
2019-03-03 17:25:56 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,15 +2,14 @@
 # out the current state.
 def display_board(board)
   separator = "\n-----------\n"
-  printed_board = board.map { |tile| " #{tile} " }
-    .map.with_index { |tile, idx| idx % 3 === 1 ? "|#{tile}|" : tile }
-    # .map.with_index { |tile, idx| [2,5].include?(idx) ? "#{tile}#{separator}" : tile }
+  printed_board = board.map! { |tile| " #{tile} " }
+    .map!.with_index { |tile, idx| idx % 3 === 1 ? "|#{tile}|" : tile }
+    .map!.with_index { |tile, idx| [2,5].include?(idx) ? "#{tile}#{separator}" : tile }
     
-  puts printed_board
     
-  # puts "#{board[0]}#{board[1]}#{board[2]}"
-  # puts "-" * 11
-  # puts "#{board[3]}#{board[4]}#{board[5]}"
-  # puts "-" * 11
-  # puts "#{board[6]}#{board[7]}#{board[8]}"
+  puts "#{board[0]}#{board[1]}#{board[2]}"
+  puts "-" * 11
+  puts "#{board[3]}#{board[4]}#{board[5]}"
+  puts "-" * 11
+  puts "#{board[6]}#{board[7]}#{board[8]}"
 end
