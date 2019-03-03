a86a67541f2c5bd4cc75e7d66783a417b4fab677
Ivan Szebenszki <szib77@gmail.com>
2019-03-03 16:41:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,8 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  console.log(board.inspect)
+  # console.log(board.inspect)
+  for square in board do
+    
+  end
 end
